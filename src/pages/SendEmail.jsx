75fdbf1787219a67adc1fb38a413dc925b190d1c
Jose_Rivera <joseriveradl@gmail.com>
2022-06-08 20:59:47 -0600

    :construction: wontainers, components and pages

--- src/pages/SendEmail.jsx
+++ src/pages/SendEmail.jsx
@@ -1,21 +1,17 @@
 import React from 'react'
-import '../styles/RecoveryPassword.scss'
+import '../styles/SendEmail.scss'
 
-const RecoveryPassword = () => {
+const SendEmail = () => {
   return (
-    <div className="login">
+    <div className="SendEmail">
       <div className="form-container">
         <img src="./logos/logo_yard_sale.svg" alt="logo" className="logo" />
-
         <h1 className="title">Email has been sent!</h1>
         <p className="subtitle">Please check your inbox for instructions on how to reset the password</p>
-
         <div className="email-image">
           <img src="./icons/email.svg" alt="email" />
         </div>
-
         <button className="primary-button login-button">Login</button>
-
         <p className="resend">
           <span>Didn't receive the email?</span>
           <a href="/">Resend</a>
@@ -25,4 +21,4 @@ const RecoveryPassword = () => {
   )
 }
 
-export default RecoveryPassword
+export default SendEmail
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from '../pages/Home'
 import Layout from '../containers/Layout'
-import Login from '../containers/Login'
+import Login from '../pages/Login'
 import NotFound from '../pages/NotFound'
-import RecoveryPassword from '../containers/RecoveryPassword'
+import PasswordRecovery from '../pages/PasswordRecovery'
+import SendEmail from '../pages/SendEmail';
+import NewPassword from '../pages/NewPassword';
+import MyAccount from '../pages/MyAccount';
+import CreateAccount from '../pages/CreateAccount';
+import CheckOut from '../pages/CheckOut';
+import Orders from '../pages/Orders';
 import '../styles/global.css'
 
 const App = () => {
@@ -14,7 +20,13 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/recovery-password" element={<RecoveryPassword />} />
+          <Route path="/recovery-password" element={<PasswordRecovery />} />
+          <Route path="/send-email" element={<SendEmail />} />
+          <Route path="/new-password" element={<NewPassword />} />
+          <Route path="/account" element={<MyAccount />} />
+          <Route path="/create-account" element={<CreateAccount />} />
+          <Route path="/checkout" element={<CheckOut />} />
+          <Route path="/orders" element={<Orders />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Layout>
